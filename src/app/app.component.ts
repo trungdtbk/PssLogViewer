ee96eb9913a8240506a55b15fba4faf57e567286
Trung Truong <trung.truong@nokia.com>
2020-09-05 13:58:52 +1200

    update title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PssLogViewer';
+  title = '1830PSS Log Viewer';
+
+  constructor(private titleSvc: Title) {
+    this.titleSvc.setTitle(this.title);
+  }
 }
