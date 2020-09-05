cb055f9af8dd6856a5cfe5ce7fc01c7512293c8c
Trung Truong <trung.truong@nokia.com>
2020-09-06 03:20:24 +1200

    test if toolbar is rendered

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -20,16 +22,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PssLogViewer'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('PssLogViewer');
-  });
-
-  it('should render title', () => {
+  it('should have the toolbar', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('PssLogViewer app is running!');
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('app-toolbar')).not.toBe(null);
   });
 });
